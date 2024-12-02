73a8dabaf3ad9f8524c44f2ad5eefb15832fb9bd
Anudari906 <anuka.tseveendorj@gmail.com>
2024-12-02 20:00:47 +0000

    new project to test

---
+++ src/git_2024636/Git_2024636.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_2024636;
+
+/**
+ *
+ * @author anudari
+ */
+public class Git_2024636 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        System.out.println("Anudari - 2024636");
+    }
+    
+}
